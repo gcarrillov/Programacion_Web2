288bab0b3d4b8b44a2427cd995c227c2f0622db3
gustavo villalta <gcarrillov@unsa.edu.pe>
2025-06-03 00:00:12 -0500

    agregando cambios para conectar todos los archivos con la aplicacion

--- Laboratorio_B/Lab07_Django-Sitios_Turisticos/destinos_project/destinos/urls.py
+++ Laboratorio_B/Lab07_Django-Sitios_Turisticos/destinos_project/destinos/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('agregar/', views.agregar_destino, name='agregar_destino'),
+    path('destinos/', views.destino_list, name='listar_destinos'),
+    path('editar/<int:id>/', views.editar_destino, name='editar_destino'),
+    path('eliminar/<int:id>/', views.eliminar_destino, name='eliminar_destino'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
--- Laboratorio_B/Lab07_Django-Sitios_Turisticos/destinos_project/destinos/views.py
+++ Laboratorio_B/Lab07_Django-Sitios_Turisticos/destinos_project/destinos/views.py
@@ -3,6 +3,34 @@ from .models import DestinosTuristicos
 from .forms import DestinoForm, RegistroForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+
+@login_required
+def destino_list(request):
+    destinos = DestinosTuristicos.objects.all()
+    return render(request, 'destinos/destino_list.html', {'destinos': destinos})
+
+@login_required
+def editar_destino(request, id):
+    destino = get_object_or_404(DestinosTuristicos, id=id)
+    if request.method == 'POST':
+        form = DestinoForm(request.POST, request.FILES, instance=destino)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_destinos')
+    else:
+        form = DestinoForm(instance=destino)
+    return render(request, 'destinos/destino_form.html', {'form': form})
+
+@login_required
+def eliminar_destino(request, id):
+    destino = get_object_or_404(DestinosTuristicos, id=id)
+    if request.method == 'POST':
+        destino.delete()
+        return redirect('listar_destinos')
+    return render(request, 'destinos/eliminar_confirmar.html', {'destino': destino})
+
 
 def home(request):
     destinos = DestinosTuristicos.objects.all()
