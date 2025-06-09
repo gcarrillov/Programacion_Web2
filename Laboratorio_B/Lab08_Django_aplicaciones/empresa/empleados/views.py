2e11c844e5d9bb830b851007d499520263cfcb1f
gustavo villalta <gcarrillov@unsa.edu.pe>
2025-06-08 23:11:05 -0500

    se creo una vista para que se pueda generar pdf

--- Laboratorio_B/Lab08_Django_aplicaciones/empresa/empleados/views.py
+++ Laboratorio_B/Lab08_Django_aplicaciones/empresa/empleados/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Empleado
+from django.template.loader import get_template
+from xhtml2pdf import pisa
 
-# Create your views here.
+def lista_empleados_pdf(request):
+    empleados = Empleado.objects.all()
+    template = get_template('empleados/pdf_empleados.html')
+    html = template.render({'empleados': empleados})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="empleados.pdf"'
+    pisa.CreatePDF(html, dest=response)
+    return response
