c11829d574177b77032a68e44a91aa4e0b13fc14
gustavo villalta <gcarrillov@unsa.edu.pe>
2025-06-28 22:31:07 -0500

    ampliando views.py de la aplicacion de personas, para las vistas basadas en clase(Django 5)

--- TeoriaA/Django4y5/listaContactos/personas/views.py
+++ TeoriaA/Django4y5/listaContactos/personas/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from .models import Persona
 from .forms import PersonaForm
 from django.shortcuts import redirect
+from django.views.generic import ListView
 
+class PersonaListView(ListView):
+    model = Persona
+    template_name = 'personas/lista_cbv.html'
+    context_object_name = 'personas'
 def lista_personas(request):
     personas = Persona.objects.all()
     return render(request, 'personas/lista.html', {'personas': personas})
