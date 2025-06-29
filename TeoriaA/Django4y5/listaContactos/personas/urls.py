61e81bd74f03418a8c9d1a2fed29b45527da4eb7
gustavo villalta <gcarrillov@unsa.edu.pe>
2025-06-28 22:35:51 -0500

    agregando contenido para urls.py para el uso correcto de las listas de personas para la aplicacion

--- TeoriaA/Django4y5/listaContactos/personas/urls.py
+++ TeoriaA/Django4y5/listaContactos/personas/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import PersonaListView
 
 urlpatterns = [
     path('', views.lista_personas, name='lista_personas'),
     path('crear/', views.crear_persona, name='crear_persona'),
+    path('lista-cbv/', PersonaListView.as_view(), name='lista_cbv'),
 ]
