e2e563025533a853601eb1def7dab1beceb00df9
gustavo villalta <gcarrillov@unsa.edu.pe>
2025-06-28 22:18:42 -0500

    se amplia archivo de views.py para la aplicacion personas

--- TeoriaA/Django4y5/listaContactos/personas/views.py
+++ TeoriaA/Django4y5/listaContactos/personas/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from .models import Persona
+from .forms import PersonaForm
+from django.shortcuts import redirect
 
 def lista_personas(request):
     personas = Persona.objects.all()
     return render(request, 'personas/lista.html', {'personas': personas})
+
+def crear_persona(request):
+    if request.method == 'POST':
+        form = PersonaForm(request.POST)
+        if form.is_valid():
+            # Guardar en la base de datos (lo haremos luego)
+            return redirect('lista_personas')
+    else:
+        form = PersonaForm()
+    return render(request, 'personas/formulario.html', {'form': form})
