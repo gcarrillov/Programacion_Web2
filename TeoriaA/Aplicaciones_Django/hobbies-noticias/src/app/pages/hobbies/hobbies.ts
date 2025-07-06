33515670e4b2948edb76443d3feec0cbd60a6191
gustavo villalta <gcarrillov@unsa.edu.pe>
2025-07-05 19:03:08 -0500

    agregando contenido para que el componente hobbies funciones, se agrego el archivo ts , el formulario html y el css del componente

--- TeoriaA/Aplicaciones_Django/hobbies-noticias/src/app/pages/hobbies/hobbies.ts
+++ TeoriaA/Aplicaciones_Django/hobbies-noticias/src/app/pages/hobbies/hobbies.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-hobbies',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './hobbies.html',
-  styleUrl: './hobbies.css'
+  styleUrls: ['./hobbies.css']
 })
 export class Hobbies {
+  nuevoHobby: string = '';
+  hobbies: string[] = [];
 
+  agregarHobby() {
+    if (this.nuevoHobby.trim()) {
+      this.hobbies.push(this.nuevoHobby.trim());
+      this.nuevoHobby = '';
+    }
+  }
+
+  eliminarHobby(index: number) {
+    this.hobbies.splice(index, 1);
+  }
 }
