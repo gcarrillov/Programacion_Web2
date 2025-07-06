86c6632a8e10c2293c183a67953250387a75cbfd
gustavo villalta <gcarrillov@unsa.edu.pe>
2025-07-05 19:11:50 -0500

    configuracion del componente news

--- TeoriaA/Aplicaciones_Django/hobbies-noticias/src/app/pages/news/news.ts
+++ TeoriaA/Aplicaciones_Django/hobbies-noticias/src/app/pages/news/news.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewsService } from '../../services/news/news';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-news',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './news.html',
-  styleUrl: './news.css'
+  styleUrls: ['./news.css']
 })
-export class News {
+export class News implements OnInit {
+  noticias: any[] = [];
 
+  constructor(private newsService: NewsService) {}
+
+  ngOnInit(): void {
+    this.newsService.getNoticias().subscribe(data => {
+      this.noticias = data;
+    });
+  }
 }
